22a1b47f5656f1e5a2f4dfb04f329d9aac2d42c7
musaraf0101 <nowsathmusaraf037@gmail.com>
2024-06-30 12:50:44 +0530

    Day 17 SQL

--- SQL/Day 17/basic.sql
+++ SQL/Day 17/basic.sql
@@ -1,1 +1,19 @@
---
+--create a database
+CREATE DATABASE school;
+
+--use database
+USE school;
+
+--show databases
+SHOW DATABASES;
+
+--drop database
+DROP DATABASE school;
+
+--create table with colum name
+create table student(
+fullname varchar(50),
+nic varchar(40),
+gender varchar(20),
+age INT
+);
