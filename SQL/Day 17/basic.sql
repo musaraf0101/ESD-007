a18b8636330fe4af1a509bea9d52608993e75483
musaraf0101 <nowsathmusaraf037@gmail.com>
2024-06-30 13:14:38 +0530

    Day 17 sql

--- SQL/Day 17/basic.sql
+++ SQL/Day 17/basic.sql
@@ -17,3 +17,21 @@ nic varchar(40),
 gender varchar(20),
 age INT
 );
+
+--view detail table
+describe student;
+
+--drop a table 
+drop student;
+
+--add new column exits table
+alter table student add column z_score double;
+
+--change column name
+alter table student change column fullname fullName varchar(40);
+
+--change data type
+alter table student change age age varchar(10);
+
+--rename table name
+alter table student rename to student_info;
